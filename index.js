3ef6c52f5ed53844d245d6f6f4ebc58b40e1fa5c
raxsonic <service@raxsonic.com>
2021-08-29 18:50:33 +0100

    Final

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const advice = [
-	'If you’re good at something, never do it for free.',
+	'If you are good at something, never do it for free.',
 	'Do, or do not. There is no try.',
-	'It’s what you do right now that makes a difference.',
+	'It is what you do right now that makes a difference.',
 	"Until you start believing in yourself, you ain't gonna have a life!",
 	'Life does not stop and start at your convenience.',
 	"You know what the trouble about real life is? There's no danger music.",
